077f66a559eab1e4da7bc4120aa173d50942de09
theatypicaldeveloper <mighty.matty91@gmail.com>
2022-02-19 14:47:05 +0000

    lesson-10 - add error snackbar

--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 
 const Storage = () => {
@@ -5,6 +6,36 @@ const Storage = () => {
         <div style={{marginLeft: '500px'}}>
             This is storage page.
         </div>
+=======
+import React, { useState, useEffect } from 'react'
+import GridWrapper from '../../components/common/GridWrapper/GridWrapper'
+import BasicSnackbar from '../../components/common/BasicSnackbar/BasicSnackbar';
+import UserTable from '../../components/UserTable/UserTable';
+import BasicCard from '../../components/common/BasicCard/BasicCard';
+
+const Storage = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+    return (
+      <GridWrapper>
+        <BasicCard 
+          content={<UserTable onError={() => setOpen(true)} />}
+        />
+        <BasicSnackbar
+          open={open}
+          severity="error"
+          message="Data couldn't be fetched"
+          onClose={handleClose}
+        />
+      </GridWrapper>
+>>>>>>> c40efda (lesson-10 - add error snackbar)
     )
 }
 
